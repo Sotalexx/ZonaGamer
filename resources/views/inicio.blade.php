b1eef6b62b2e2360fbf35e7bac47491d0581108c
waldir21 <vr19030@ues.edu.sv>
2023-11-18 22:16:22 -0600

    nuevos cambios diseño

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,51 +1,173 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <title>Terreneitor-Games</title>
- 
+
+       @vite(['resources/css/inicio.css'])
+
+    <title>ZonaGamer</title>
 </head>
 <body>
 
-<!-- Barra de Navegación Principal -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-gray">
     <a class="navbar-brand" href="#">
-        <img src="logo.png" alt="Logo" style="max-height: 30px;">
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" height="80">
     </a>
-    <form class="form-inline my-2 my-lg-0 mr-auto">
-        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-    </form>
-    <ul class="navbar-nav ml-auto">
     
-        <li class="nav-item">
-        <a class="nav-link" href="{{ url('/login') }}"><i class="fa-regular fa-user"></i>Login</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ url('/register') }}">Registro</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i>Carrito</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Ayuda</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Información</a>
-        </li>
-    </ul>
+    <form class="form-inline mx-auto ">
+        <input class="form-control" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-secondary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
+
+    
+
+    <div class="collapse navbar-collapse justify-content-end ">
+        <ul class="navbar-nav ">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#"><i class="fa-solid fa-house"></i>Inicio</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white" href="{{ url('/login') }}"><i class="fa-regular fa-user"></i>Iniciar Seccion</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white" href="{{ url('/register') }}">Registro</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#"><i class="fa-solid fa-cart-shopping"></i>Carrito</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#"></li><i class="fa-regular fa-credit-card"></i>Pagar</a>
+            
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Perfil</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<hr class="separador">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-grey">
+    <div class="collapse navbar-collapse justify-content-center">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#"><i class="fa-solid fa-bars"></i>Categorías</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Juegos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Marcas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Prendas + Accesorios</a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
-<!-- Barra de Navegación de Categorías -->
-<nav class="navbar navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#categoryNavbar"
-            aria-controls="categoryNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fa-solid fa-bars"> CATEGORIAS</i>
-    </button>
-</nav>
+<br>
+<!-- Carrusel centrado con información al lado derecho -->
+<p class="text-center">DESTACADOS</p>
+<div class="container mt-4">
+    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="3000">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image1.jpg') }}" class="d-block w-50" alt="Imagen 1 ">
+                    <div class="ml-4">
+                        <p>Con este título innovador y auténtico, <br>
+                         te sentirás mucho más cerca del juego <br>
+                          gracias a una experiencia futbolística más real que nunca</p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image2.jpg') }}" class="d-block w-50" alt="Imagen 2">
+                    <div class="ml-4">
+                        <p>Inicia de la mejor manera con <br>
+                         Call of Duty: Modern Warfare 3 
+                        <br>un emocionante videojuego de disparos en primera persona </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image3.jpg') }}" class="d-block w-50" alt="Imagen 2">
+                    <div class="ml-4">
+                        
+                        <p>Alan Wake II 
+                           <br> es un videojuego de horror de supervivencia </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image4.jpg') }}" class="d-block w-50" alt="Imagen 2">
+                    <div class="ml-4">
+                        <p>Una aventura épica a través de la tierra y los cielos te espera en <br>
+                          The Legend of Zelda™: Tears of the Kingdom <br>. 
+                        La aventura es tuya para crearla en un mundo impulsado por tu imaginación</p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image5.png') }}" class="d-block w-50" alt="Imagen 2">
+                    <div class="ml-4">
+                        <p>Los Spider-Men Peter Parker y Miles Morales <br>
+                        regresan para una nueva y emocionante aventura <br>
+                         de la aclamada franquicia Marvel's Spider-Man </p>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('img/image6.jpg') }}" class="d-block w-50" alt="Imagen 2">
+                    <div class="ml-4">
+                        <p>Super Mario Bros. Wonder', <br>
+                         una aventura con más personajes, <br>
+                         más transformaciones y más maravillas que nunca</p>
+                    </div>
+                </div>
+            </div>
+            <!-- Agrega más imágenes -->
+        </div>
+        <a class="carousel-control-prev ml-n3" href="#carouselExample" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+</div>
+
+<!-- Footer -->
+<footer class="bg-dark text-light py-3 mt-5 text-center">
+        <p>&copy; 2023 <img src="{{ asset('img/logo.png') }}" alt="Logo" height="80"></p>
+        <p>
+            <a href="#" class="text-light"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="text-light ml-3"><i class="fab fa-instagram"></i></a>
+            <a href="tel:+123456789" class="text-light ml-3"><i class="fas fa-phone"></i></a>
+        </p>
+        <p><a href="#">Política de privacidad</a> | <a href="#">Términos y condiciones</a></p>
+</footer>
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/style.css">
+   
+    @vite(['resources/css/login.css'])
+
     <title>Login</title>
 </head>
 <body>
@@ -32,7 +34,7 @@
                   </div>
 
                   <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Acceder</button>
+                    <button class=" btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Acceder</button>
                     <a class="text-muted" href="#!">¿Has olviadado tu contraseña?</a>
                   </div>
 
