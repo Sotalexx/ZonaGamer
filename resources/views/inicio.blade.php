57026afac2c438b6539822f84be4a0ae3b83f2bb
waldir21 <vr19030@ues.edu.sv>
2023-11-22 13:25:04 -0600

    mas diseño

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -22,14 +22,11 @@
   </div>
 
   <!--Barra de busqueda-->
-  <div class="contenedor-barra">
-    <div class="barra-container">
-         <input class="buscar" type="text" placeholder="Buscar">
-        <div class="btn-barra">
-             <i class="fas fa-search"></i>
-         </div>
-    </div>
-    </div>
+  
+    <form class="form-inline mx-auto ">
+        <input class="form-controlBuscar m-3 p-2" type="search" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-secondary p-2" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
 
  <!--Botones de interaccion-->
     <div class="btn-interaccion">
@@ -85,22 +82,100 @@
                  </ul>
             </div>
           </div>
+
+          <div class="dropdown "> 
+            <a class="text-light" href="{{ url('/noticias') }}">Noticias</a>
+
+          </div>
+
+
         </navbar> 
     
 
          <!--Banner principal-->
          <div class="logo-mamamlon">
-            <img src="{{asset('img/banner-maalon.jpg')}}" alt="">
-         </div>
+            <img src="{{asset('img/Banner-Grande.png')}}" alt="">
+         </div> <br><br><br>
 
-         <!--Tendecias de productos-->
-         <div class="container-tendencia">
-            <div class="tendecia">
-                <h2>Tendencias</h2><hr>
-            </div>
-         </div>
+        <h2 class=" text-light text-center" > DESTACADOS</h2>
+   <div id="myCarousel" class="carousel slide" data-ride="carousel">
+     <div class="carousel-inner">
+    <!-- Primera imagen -->
+    <div class="carousel-item active">
+      <img src="{{asset('img/image1.jpg')}}" class="d-block w-100" alt="Imagen 1">
+      <div class="carousel-caption">
+        <p>Con este título innovador y auténtico, <br>
+                         te sentirás mucho más cerca del juego <br>
+                          gracias a una experiencia futbolística más real que nunca</p>
+      </div>
+    </div>
+    <!-- Segunda imagen -->
+    <div class="carousel-item">
+      <img src="{{asset('img/image2.jpg')}}" class="d-block w-100" alt="Imagen 2">
+      <div class="carousel-caption">
+        <p>nicia de la mejor manera con <br>
+                         Call of Duty: Modern Warfare 3 
+                        <br>un emocionante videojuego de disparos en primera persona </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/image5.png')}}" class="d-block w-100" alt="Imagen 2">
+      <div class="carousel-caption">
+        <p>Los Spider-Men Peter Parker y Miles Morales <br>
+                        regresan para una nueva y emocionante aventura <br>
+                         de la aclamada franquicia Marvel's Spider-Man </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/image6.jpg')}}" class="d-block w-100" alt="Imagen 2">
+      <div class="carousel-caption">
+        <p>Super Mario Bros. Wonder', <br>
+                         una aventura con más personajes, <br>
+                         más transformaciones y más maravillas que nunca</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/image3.jpg')}}" class="d-block w-100" alt="Imagen 2">
+      <div class="carousel-caption">
+        <p>Alan Wake II 
+         <br> es un videojuego de horror de supervivencia</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/image4.jpg')}}" class="d-block w-100" alt="Imagen 2">
+      <div class="carousel-caption">
+        <p>Una aventura épica a través de la tierra y los cielos te espera en <br>
+                          The Legend of Zelda™: Tears of the Kingdom <br>. 
+                        La aventura es tuya para crearla en un mundo impulsado por tu imaginació</p>
+      </div>
+    </div>
 
 
+
+  </div>
+  <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Siguiente</span>
+  </a>
+</div>
+
+
+<h2>PARA QUE SALGAN LOS JUEGSO</h2>
+
+<!-- Footer -->
+<footer class="bg-dark text-light py-3 mt-5 text-center">
+        <p>&copy; 2023 <img src="{{ asset('img/logo.png') }}" alt="Logo" height="80"></p>
+        <p>
+            <a href="#" class="text-light"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="text-light ml-3"><i class="fab fa-instagram"></i></a>
+            <a href="tel:+123456789" class="text-light ml-3"><i class="fas fa-phone"></i></a>
+        </p>
+        <p><a href="#">Política de privacidad</a> | <a href="#">Términos y condiciones</a></p>
+</footer>
 
 
 
